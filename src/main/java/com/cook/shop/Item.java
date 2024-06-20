50a8aad0db64d4c8108b391a75e1b3ccd4903c54
Yunguk Kim <kug1328@naver.com>
2024-06-21 01:40:16 +0900

    html modified

---
+++ src/main/java/com/cook/shop/Item.java
@@ -0,0 +1,16 @@
+package com.cook.shop;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    public String title;
+
+    @Column
+    public Integer price;
+}
